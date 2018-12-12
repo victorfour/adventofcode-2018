dbcbf82abfb219718160ae63760f3cd02bc92d75
victor <victor.four@gmail.com>
2018-12-12 19:22:10 +0100

    [day10] part1

--- src/adventofcode_2018/day10.clj
+++ src/adventofcode_2018/day10.clj
@@ -6,17 +6,11 @@
 ;;; part 1
 
 
-(def exampletxt "./resources/day10/example.txt")
-(def inputtxt "./resources/day10/input.txt")
-
 (defn parse-line
   [line]
-  (let [
-        pattern #"position=<\s*(-?\d+),\s*(-?\d+)> velocity=<\s*(-?\d+),\s*(-?\d+).+"
-        found   (rest (re-find pattern line))
-        ]
-    (map #(Long/parseLong %) found)
-        ))
+  (let [pattern #"position=<\s*(-?\d+),\s*(-?\d+)> velocity=<\s*(-?\d+),\s*(-?\d+).+"
+        found   (rest (re-find pattern line))]
+    (map #(Long/parseLong %) found)))
 
 
 (defn parse-data
@@ -34,20 +28,21 @@
   [velocities positions]
   (m/add positions velocities))
 
+
 (def on \#)
 (def off \.)
 
+
 (defn fill-grid
   [positions]
-  (let [
-        y-max (apply max (second positions))
+  (let [y-max (apply max (second positions))
         y-min (apply min (second positions))
         x-max (apply max (first positions))
         x-min (apply min (first positions))
-        x-offset (Math/abs x-min)
-        y-offset (Math/abs y-min)
-        x-range (inc (- x-max x-min))
-        y-range (inc (- y-max y-min))
+        x-offset (- x-min)
+        y-offset (- y-min)
+        x-range (inc (Math/abs (- x-max x-min)))
+        y-range (inc (Math/abs (- y-max y-min)))
         offset-positions [(m/add x-offset (first positions)) (m/add y-offset (second positions))]
         pos (m/transpose offset-positions)
         grid (m/mutable (partition x-range (vec (repeat (* x-range y-range) off))))]
@@ -61,15 +56,13 @@
 
 (defn get-range
   [positions]
-  (let [
-        y-max (apply max (second positions))
+  (let [y-max (apply max (second positions))
         y-min (apply min (second positions))
         x-max (apply max (first positions))
         x-min (apply min (first positions))
         x-range (inc (- x-max x-min))
         y-range (inc (- y-max y-min))]
-    [x-range y-range]
-    ))
+    [x-range y-range]))
 
 
 (defn display
@@ -77,12 +70,8 @@
   (let [grid (fill-grid positions)
         [height width] (m/shape grid)]
     (dotimes [n height]
-      (println (nth grid n)))))
+      (println (into [] (nth grid n))))))
 
-
-(time (let [[positions velocities] (parse-data "./resources/day10/example.txt")
-      step (partial next-second velocities)]
-  (println (display (last (take 4 (iterate step positions)))))))
 
 (defn part1
   [input]
@@ -98,8 +87,8 @@
           (recur next-positions
                  next-range))))))
 
-(part1 "./resources/day10/input.txt")
-      
+(time (part1 "./resources/day10/input.txt"))
+;;;=>BLGNHPJC
+;;;"Elapsed time: 5846.092981 msecs"
 
-  
 
