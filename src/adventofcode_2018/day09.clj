bae787cff6050191d827275750c04a350e35cc75
Victor FOUR <vfour@student.42.fr>
2018-12-12 10:08:11 +0100

    [day09] part1: some optimizations

--- src/adventofcode_2018/day09.clj
+++ src/adventofcode_2018/day09.clj
@@ -7,7 +7,8 @@
 
 (defn init-state
   [num-players]
-  {:num-players num-players
+  {:num-marbles 1
+   :num-players num-players
    :circle [0]
    :current-marble-pos 0
    :current-marble-val 1 
@@ -16,39 +17,50 @@
 
 
 (defn next-pos
-  [{:keys [current-marble-pos circle] :as state}]
-  (let [pos (mod (+ current-marble-pos 2) (count circle))]
+  [{:keys [current-marble-pos num-marbles] :as state}]
+  (let [pos (mod (+ current-marble-pos 2) num-marbles)]
     (if (zero? pos)
-      (count circle)
+      num-marbles
       pos)))
 
 
+(let [circle '(1 2 3 4 5)
+      [head tail] (split-at 2 circle)]
+  (println head)
+  (println tail)
+  (println (flatten (conj tail 42 head)))
+  )
+      
+
 (defn place-marble
-  [{:keys [scores circle current-player current-marble-val num-players] :as state}]
+  [{:keys [scores circle current-player current-marble-pos current-marble-val num-players num-marbles] :as state}]
   (let [new-marble-val (inc current-marble-val)
         pos (next-pos state)
         [head tail] (split-at pos circle)
-        new-tail (conj tail current-marble-val)
-        new-circle (concat head new-tail)
-        new-current-player (mod (inc current-player) num-players)]
+        new-circle (apply conj tail current-marble-val (reverse head))
+        ]
     (-> state
+        (assoc :num-marbles (inc num-marbles))
+        (assoc :current-marble-val (inc current-marble-val))
+        (assoc :current-player (mod (inc current-player) num-players))
+
         (assoc :circle new-circle)
-        (assoc :current-marble-val new-marble-val)
         (assoc :current-marble-pos pos)
-        (assoc :current-player new-current-player))))
+        )))
 
 
 (defn remove-marble
-  [{:keys [scores circle num-players current-player current-marble-pos current-marble-val] :as state}]
+  [{:keys [scores circle num-players current-player num-marbles current-marble-pos current-marble-val] :as state}]
   (let [new-marble-val (inc current-marble-val)
-        to-remove-pos (mod (- current-marble-pos 7) (count circle))
-        new-current-marble-pos (mod to-remove-pos (dec (count circle)))
+        to-remove-pos (mod (- current-marble-pos 7) num-marbles)
+        new-current-marble-pos (mod to-remove-pos (dec num-marbles))
         points  (+ (nth scores current-player) (nth circle to-remove-pos) current-marble-val)
         [head tail] (split-at to-remove-pos circle)
         new-circle (concat head (rest tail))
         new-current-player (mod (inc current-player) num-players)
         new-scores (assoc scores current-player points)]
     (-> state
+        (assoc :num-marbles (dec num-marbles))
         (assoc :circle new-circle)
         (assoc :current-marble-val new-marble-val)
         (assoc :current-marble-pos new-current-marble-pos)
@@ -77,9 +89,9 @@
 (time (part1 30 5807))
 (time (println (part1 452 71250)))
 ;;;=>388844
-;;;"Elapsed time: 658258.725558 msecs"
+;;;"Elapsed time: 383323.694446 msecs"
 
-;;; part2 is (part1 452 71250), but that's gonna take > 18 hours with this code...
+;;; part2 is (part1 452 7125000), but that's gonna take > 6 hours with this code...
 
   
   
