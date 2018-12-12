16bd2998f8f271232839dcba84f1e00911eca256
Victor FOUR <vfour@student.42.fr>
2018-12-12 13:42:49 +0100

    [day10] part1: needs to detect when to stop iterating

---
+++ src/adventofcode_2018/day10.clj
@@ -0,0 +1,74 @@
+(ns adventofcode-2018.day10
+  (:require [clojure.string :as str]
+            [clojure.pprint :as pp]
+            [clojure.core.matrix :as m]))
+            
+;;; part 1
+
+
+(def exampletxt "./resources/day10/example.txt")
+(def inputtxt "./resources/day10/input.txt")
+
+(defn parse-line
+  [line]
+  (let [
+        pattern #"position=<\s*(-?\d+),\s*(-?\d+)> velocity=<\s*(-?\d+),\s*(-?\d+).+"
+        found   (rest (re-find pattern line))
+        ]
+    (map #(Long/parseLong %) found)
+        ))
+
+
+(defn parse-data
+  [input]
+  (let [[positions velocities ] (-> (slurp input)
+                                    (str/trim)
+                                    (str/split-lines)
+                                    (->> (map parse-line)
+                                         (m/transpose)
+                                         (partition 2)))]
+    [positions velocities]))
+
+
+(defn next-second
+  [velocities positions]
+  (m/add positions velocities))
+
+(def on 1)
+(def off 0)
+
+(defn fill-grid
+  [positions]
+  (let [
+        y-max (apply max (second positions))
+        y-min (apply min (second positions))
+        x-max (apply max (first positions))
+        x-min (apply min (first positions))
+        x-offset (Math/abs x-min)
+        y-offset (Math/abs y-min)
+        x-range (inc (- x-max x-min))
+        y-range (inc (- y-max y-min))
+        offset-positions [(m/add x-offset (first positions)) (m/add y-offset (second positions))]
+        pos (m/transpose offset-positions)
+        grid (m/mutable (partition x-range (vec (repeat (* x-range y-range) off))))]
+    (loop [points pos]
+      (if (empty? points)
+        grid
+        (let [point (first points)]
+          (m/mset! grid (second point) (first point) on)
+          (recur (rest points)))))))
+
+
+(defn display
+  [positions]
+  (let [grid (fill-grid positions)
+        [height width] (m/shape grid)]
+    (dotimes [n height]
+      (println (nth grid n)))))
+
+
+(time (let [[positions velocities] (parse-data "./resources/day10/example.txt")
+      step (partial next-second velocities)]
+  (println (display (last (take 4 (iterate step positions)))))))
+  
+
