3ee28af19bfb08809bd6e5eb81f8e1a4cf762fdf
Victor FOUR <vfour@student.42.fr>
2018-12-13 10:54:29 +0100

    [day11] part 1

---
+++ src/adventofcode_2018/day11.clj
@@ -0,0 +1,99 @@
+(ns adventofcode-2018.day11
+  (:require [clojure.string :as str]
+            [clojure.pprint :as pp]
+            [clojure.core.matrix :as m]))
+
+
+;;; part 1
+
+;;x,y notation,
+;; top-left cell  : (  1, 1)
+;; top-right cell : (300, 1)
+
+;; for computation, using  top-left  (  0, 0)
+;;                         top-right (299, 0)
+
+
+
+(defn hundreds
+  [n]
+  (-> (nth (reverse (str n)) 2 \0)
+      (int)
+      (- (int \0))))
+
+
+(defn power-level
+  [serial-number coords]
+  (let [rack-id (+ (first coords) 10)]
+    (-> rack-id
+        (* (second coords))
+        (+ serial-number)
+        (* rack-id)
+        (hundreds)
+        (- 5))))
+
+
+(power-level 8 [3 5])
+(power-level 57 [122 79])
+(power-level 39 [217 196])
+(power-level 71 [101 153])
+
+
+(defn compute-grid
+  [width height serial-number]
+  (let [cell-power (partial power-level serial-number)]
+    (->> (for [y (range height)
+               x (range width)];; first coords (x) is iterated first when scanning lines
+           (cell-power [x y]))
+         (partition width))))
+
+
+(defn conv-3x3
+  [grid [x y]]
+  (let [get-line #(nth grid %)
+        line-conv (fn [line]
+                    (reduce + (take 3 (drop x line))))]
+    (->> (range y (+ y 3))
+         (map get-line)
+         (map line-conv)
+         (reduce +))))
+
+
+(defn conv-grid
+  [grid]
+  (let [width (- (count (first grid)) 2)
+        height (- (count grid) 2)
+        conv-filter (partial conv-3x3 grid)]
+    (for [y (range height)
+          x (range width)]
+      (conv-filter [x y]))))
+
+
+(defn argmax
+  [input]
+  (->> input
+       (interleave (range (count input)))
+       (partition 2)
+       (apply max-key second)
+       (first)))
+
+
+(defn part1
+  [serial-number width height]
+  (let [conv-width (- width 2)
+        conv-height (- height 2)
+        conved-grid (conv-grid (compute-grid width height serial-number))
+        index-max-conved (argmax conved-grid)
+        x-max (mod index-max-conved conv-width)
+        y-max (/ (- index-max-conved x-max) conv-width)]
+    [x-max y-max]))
+    
+
+(println (part1 18 300 300))
+(println (part1 42 300 300))
+(time (println (part1 8868 300 300)))
+;;; [241 40]
+;;;"Elapsed time: 4536.392848 msecs"
+
+
+
