ad947864a4d0d10913fc9733ee07e594d43d47f2
victor <victor.four@gmail.com>
2018-12-28 14:54:13 +0100

    [day14] part 2

--- src/adventofcode_2018/day14.clj
+++ src/adventofcode_2018/day14.clj
@@ -5,9 +5,9 @@
 ;; part 1
 (defn init-state
   []
-  {:recipes [3 7 1 0 1 0]
-   :felf 4
-   :self 3})
+  {:recipes [3 7 1 0 1 0 1 2]
+   :felf 0
+   :self 6})
 
 
 (defn concat-recipes
@@ -40,6 +40,24 @@
 (time (println (part1 236021)))
 ;;=>6297310862
 ;;=>"Elapsed time: 482.717829 msecs"
-
 ;;when using a string to store recipes : "Elapsed time: 7710.605768 msecs"
 
+
+;; part2
+
+(defn part2
+  [input]
+  (let [pattern (mapv #(Long/parseLong (str %)) (seq input))
+        pattern-len (count pattern)]
+    (loop [state (init-state)]
+      (let [start-scan (- (count (:recipes state)) pattern-len)
+            candidate1 (subvec (:recipes state) start-scan (+ start-scan pattern-len))
+            candidate2 (subvec (:recipes state) (dec start-scan) (+ (dec start-scan) pattern-len))]
+        (cond
+          (= pattern candidate1) start-scan
+          (= pattern candidate2) (dec start-scan)
+          :else (recur (make state)))))))
+      
+(time (println (part2 "236021")))
+;;=>20221334
+;;"Elapsed time: 45571.993285 msecs"
